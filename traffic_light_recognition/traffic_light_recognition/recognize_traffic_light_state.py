ae5947ebeb431e3479911fea3dc89e39c66c4dc7
p1xta <daria.petrova496@gmail.com>
2025-12-22 15:35:34 +0700

    fix: shut down once

--- traffic_light_recognition/traffic_light_recognition/recognize_traffic_light_state.py
+++ traffic_light_recognition/traffic_light_recognition/recognize_traffic_light_state.py
@@ -70,13 +70,12 @@ class TrafficLightDetector(Node):
 
         if state == TrafficLightState.GREEN:
             self.get_logger().info("Traffic light GREEN detected")
-
+            self.destroy_subscription(self.subscription)
             self.finished = True
             self.create_timer(0.5, self.shutdown)
 
     def shutdown(self):
         self.get_logger().info("Traffic light estimator finished")
-        self.destroy_subscription(self.subscription)
         self.destroy_node()
         rclpy.shutdown()
 
