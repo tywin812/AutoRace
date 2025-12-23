1b549393a4fce2658c76b6d20db9d89ce4a2ae94
tywin812 <chrisz0r@mail.ru>
2025-12-23 10:20:32 +0700

    feat: hardcoded costruction traj

--- construction_avoidance/setup.py
+++ construction_avoidance/setup.py
@@ -1,8 +1,6 @@
 from setuptools import find_packages, setup
-import os
-from glob import glob
 
-package_name = 'autorace_core_erevan'
+package_name = 'construction_avoidance'
 
 setup(
     name=package_name,
@@ -12,8 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'),
-            glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'race_controller = autorace_core_erevan.race_controller:main'
+            'constraction_avoidance = construction_avoidance.constraction_avoidance:main',
         ],
     },
 )
